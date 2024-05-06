f89449459d5f66085b2b5161c5ded513615eba2b
z00579768 <junwei.zhao@huawei.com>
2024-05-06 09:44:07 +0800

    【Title】:cmake，工程提交 【DTS&Issues&E2E】: 【Description】: 【Root Cause】: 【Solution】: 【Impact Scope】: 【Interface】: 【Document】: 【Self Test Report】:

---
+++ cplusplus/README.md
@@ -0,0 +1,2 @@
+# learncpp
+个人C++语言学习工程，基于Clion
---
+++ cplusplus/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "leetcode.h"
+#include "leetcode_testcase.h"
+#include "exercise_testcase.h"
+#include "learning_testcase.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    LeetCode_TestCase_Main();
+    Learning_TestCase_Main();
+    Exercise_TestCase_Main();
+}
